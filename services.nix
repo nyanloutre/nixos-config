e4cf6a2da9405bcd90c2aa05472700af1b9b674e
nyanloutre <paul@nyanlout.re>
2018-04-12 09:08:45 +0200

    Port Slimserver manquant

--- services.nix
+++ services.nix
@@ -172,7 +172,7 @@ backend ${name}-backend
   networking.firewall.allowedTCPPorts = [
     80 443 # HAProxy
 #    111 2049 4000 4001 4002 # NFS
-    3483 9000 # Slimserver
+    3483 9000 9090 # Slimserver
     51413 # Transmission
   ];
   networking.firewall.allowedUDPPorts = [
