2832c54fdc251a97f295e0152d8084e1e4ca1476
nyanloutre <paul@nyanlout.re>
2018-04-22 12:29:34 +0200

    Config Riot

--- services.nix
+++ services.nix
@@ -96,10 +96,18 @@ in
   services.searx.enable = true;
 
   services.nginx.enable = true;
+  nixpkgs.overlays = [ (self: super: { riot-web_custom = super.riot-web.override { conf = ''
+    {
+      "default_hs_url": "https://matrix.nyanlout.re",
+      "default_is_url": "https://vector.im",
+      "brand": "Nyanloutre",
+      "default_theme": "dark"
+    }
+  ''; }; } ) ];
   services.nginx.virtualHosts = {
     "riot" = {
       listen = [ { addr = "127.0.0.1"; port = riot_port; } ];
-      locations = { "/" = { root = pkgs.riot-web; }; };
+      locations = { "/" = { root = pkgs.riot-web_custom; }; };
     };
   };
 
